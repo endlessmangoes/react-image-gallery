78c539672c2b00912f21b73e9a0ec565cbc534ef
aniketdatta <aniketdatta47@gmail.com>
2016-12-10 17:46:17 +0000

    renamed grid.jsx to layout.jsx - updated webpack config respectively

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-	entry: APP_DIR + '/grid.jsx',
+	entry: APP_DIR + '/Layout.jsx',
 	output: {
 		path: BUILD_DIR,
 		filename: 'bundle.js'
