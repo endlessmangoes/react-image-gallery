9474cc65c3ecd1152496cbf3120aeb3893406a20
Aniket Datta <aniket@smartthing.co.uk>
2017-01-24 16:20:10 +0000

    seperated header to its own component, added react router to abstract app using Router

--- src/client/app/Header.jsx
+++ src/client/app/Header.jsx
@@ -3,7 +3,27 @@ import {render} from 'react-dom';
 
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <header className="mdl-layout__header">
+        <div className="mdl-layout__header-row">
+          <div className="mdl-layout-spacer"></div>
+          <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+            <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="search">
+              <i className="material-icons">search</i>
+            </label>
+            <div className="mdl-textfield__expandable-holder" id="search-container">
+              <SearchInput tags={tags} onChange={this.searchInputChange}/>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 default export Header;
